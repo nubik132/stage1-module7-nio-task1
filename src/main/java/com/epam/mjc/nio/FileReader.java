e60280980032ba2defd815d184d91e36824d2e27
nubik132 <mister.nubik@list.ru>
2023-11-28 19:41:55 +0300

    feat: implement getDataFromFile method

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,27 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        try (java.io.FileReader in = new java.io.FileReader(file)) {
+            BufferedReader br = new BufferedReader(in);
+
+            String name = getSecondSplitPart(br.readLine());
+            int age = Integer.parseInt(getSecondSplitPart(br.readLine()));
+            String email = getSecondSplitPart(br.readLine());
+            Long phone = Long.parseLong(getSecondSplitPart(br.readLine()));
+
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Profile();
+    }
+
+    String getSecondSplitPart(String str){
+        return str.split("\\s")[1];
     }
 }
